5731b778a3f0a8d76d9296dd21e0e9dde14826fb
Kelechi Nwanna <91472778+knwanna9@users.noreply.github.com>
2021-09-27 21:04:48 +0000

    c

--- lab1.py
+++ lab1.py
@@ -9,15 +9,15 @@ GPIO.setup(12, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 
 def call_back(pin):
-  pwm = GPIO.PWM(16, 100) # create PWM object @ 100 Hz 
-  try:
-    pwm.start(0)                  # initiate PWM at 0% duty cycle
+print('No boi')
+'''pwm = GPIO.PWM(16, 100) # create PWM object @ 100 Hz 
+ # initiate PWM at 0% duty cycle
     while 1:
       for dc in range(101):       # loop duty cycle from 0 to 100  
         pwm.ChangeDutyCycle(dc)   # set duty cycle  
         sleep(0.01)               # sleep 10 ms
   except KeyboardInterrupt:       # stop gracefully on ctrl-C
-    print('\nExiting')
+    print('\nExiting')'''
 
 
 GPIO.add_event_detect(5, GPIO.RISING, callback=call_back, bouncetime=100)
