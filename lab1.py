1205d6e8465b24b33682545b0d3d0df73f18a939
Kelechi Nwanna <91472778+knwanna9@users.noreply.github.com>
2021-09-27 20:55:32 +0000

    f

--- lab1.py
+++ lab1.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 from time import sleep
-GPIO.cleanup()
+
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(4, GPIO.OUT)
@@ -23,7 +23,6 @@ def call_back(pin):
   GPIO.cleanup()
 
 GPIO.add_event_detect(5, GPIO.RISING, callback=call_back, bouncetime=100)
-GPIO.add_event_detect(6, GPIO.RISING, callback=call_back, bouncetime=100)
 
 while True:
   try:
