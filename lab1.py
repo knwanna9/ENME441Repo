63ac5e35a68ca29a303bd581bad5728f4d7423ad
Kelechi Nwanna <91472778+knwanna9@users.noreply.github.com>
2021-09-28 21:42:20 +0000

    f

--- lab1.py
+++ lab1.py
@@ -14,7 +14,7 @@ def call_back(pin):
   pwm.start(0)     # initiate PWM at 0% duty cycle
   for dc in range(101)+range(100,-1,-1):       # loop duty cycle from 0 to 100  
     pwm.ChangeDutyCycle(dc)   # set duty cycle  
-    sleep(0.01) 
+    sleep(0.005) 
  
 
 
