6f14463b28b7f9f0ca73e9a7a7599c5082887091
Kelechi Nwanna <91472778+knwanna9@users.noreply.github.com>
2021-10-12 20:38:59 +0000

    f

--- ADCthing.py
+++ ADCthing.py
@@ -29,11 +29,11 @@ class Joystick:
  
   def getX(self):
     X = self.pcf8591.read(0)
-    Joystick.pcf8591.write(X)
+    self.pcf8591.write(X)
     return X
   
   def getY(self):
     Y = self.pcf8591.read(1)
-    Joystick.pcf8591.write(Y)
+    self.pcf8591.write(Y)
     return Y
 		 
